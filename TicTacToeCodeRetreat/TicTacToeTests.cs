fca918ceadd9d0979eaf49f985064ab761dcd296
Kevin De Bruyn <Kevin.debruyn@protime.eu>
2021-05-04 15:43:38 +0200

    Add project files.

---
+++ TicTacToeCodeRetreat/TicTacToeTests.cs
@@ -0,0 +1,143 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TicTacToeCodeRetreat
+{
+    [TestFixture]
+    public class TicTacToeTests
+    {
+        private Game _game;
+
+        [SetUp]
+        public void Setup()
+        {
+            _game = new Game();
+        }
+
+        [Test]
+        public void Game_WhenCreating_HasTwoPlayers()
+        {
+            Assert.That(_game.Players.Count, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Game_WhenCreating_EachPlayerHasDifferentCharacter()
+        {
+            Assert.That(_game.Players.Select(player => player.Character), Is.EquivalentTo(_game.Players.Select(player => player.Character).Distinct()));
+        }
+
+        [Test]
+        public void Game_WhenCreating_AssignsCurrentPlayer()
+        {
+            Assert.That(_game.CurrentPlayer, Is.Not.Null);
+        }
+
+        [Test]
+        public void Game_WhenCreating_InitializedABoard()
+        {
+            Assert.That(_game.Board, Is.Not.Null);
+        }
+
+        [Test]
+        public void Game_WhenCreating_InitializedBoardIsEmpty()
+        {
+            Assert.That(_game.Board.Slots, Is.EquivalentTo(new string[3,3]));
+        }
+
+        [Test]
+        public void Game_StartTurn_PutsACharacterOnTheBoard()
+        {
+            _game.StartTurn();
+
+            Assert.That(_game.Board.Slots, Is.Not.EquivalentTo(new string[3, 3]))
+        }
+
+        [Test]
+        public void Game_StartTurn_PutsTheCurrentPlayersCharacterOnTheBoard()
+        {
+            _game.StartTurn();
+
+            Assert.That(_game.Board.Slots., Is.Not.EquivalentTo(new string[3, 3]))
+        }
+    }
+
+    public class Game
+    {
+        public Game()
+        {
+            CreatePlayers();
+            SetCurrentPlayer();
+            InitializeBoard();
+        }
+
+        public void StartTurn()
+        {
+            ExecuteMove();
+
+        }
+
+        public void ExecuteMove()
+        {
+            var posX = new System.Random().Next(0, 2);
+            var posY = new System.Random().Next(0, 2);
+
+            if (!Board.TryAddCharacter(posX, posY, CurrentPlayer.Character))
+                ExecuteMove();
+        }
+
+
+        private void SetCurrentPlayer()
+        {
+            CurrentPlayer = Players.First();
+        }
+
+        private void CreatePlayers()
+        {
+            Players = new List<Player>
+            {
+                new Player("O"),
+                new Player("X")
+            };
+        }
+
+        private void InitializeBoard()
+        {
+            Board = new Board();
+        }
+
+        
+
+        public Board Board { get; private set; }
+        public List<Player> Players { get; private set; }
+        public Player CurrentPlayer { get; private set;  }
+    }
+
+    public class Board
+    {
+        public string[,] Slots { get; private set; }
+
+        public Board()
+        {
+            Slots = new string[3,3];
+        }
+
+        public bool TryAddCharacter(int posX, int posY, string character)
+        {
+            if (Slots[posX, posY] != null) return false;
+
+            Slots[posX, posY] = character;
+            return true;
+        }
+    }
+
+    public struct Player
+    {
+        public string Character { get; }
+
+        public Player(string character)
+        {
+            Character = character;
+        }
+    }
+}
---
+++ TicTacToeCodeRetreatPart2/Tests.cs
@@ -0,0 +1,116 @@
+using NUnit.Framework;
+using System.Linq;
+
+namespace TicTacToeCodeRetreatPart2
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void GameCreated_EachPlayerHasDifferentCharacter()
+        {
+            var game = new Game();
+
+            Assert.That(game.Players.Select(player => player.Character), Is.EquivalentTo(game.Players.Select(player => player.Character).Distinct()));
+        }
+
+        [Test]
+        public void GameCreated_HasOneActivePlayer()
+        {
+            var game = new Game();
+
+            Assert.That(game.ActivePlayer, Is.Not.Null);
+        }
+
+        [Test]
+        public void GameCreated_AllSpotsAvailable()
+        {
+            var game = new Game();
+
+            Assert.That(game.Spots.All(spot => spot.Available), Is.True);
+        }
+
+        [Test]
+        public void GameCreated_SpotsMakeUp3By3Grid()
+        {
+            var game = new Game();
+
+            Assert.That(game.Spots, Is.EquivalentTo(new[]
+            {
+                new Spot() { Row = 1, Column = 1},
+                new Spot() { Row = 1, Column = 2},
+                new Spot() { Row = 1, Column = 3},
+                new Spot() { Row = 2, Column = 1},
+                new Spot() { Row = 2, Column = 2},
+                new Spot() { Row = 2, Column = 3},
+                new Spot() { Row = 3, Column = 1},
+                new Spot() { Row = 3, Column = 2},
+                new Spot() { Row = 3, Column = 3},
+            }));
+        }
+    }
+
+    public class Game
+    {
+        public Player[] Players { get; }
+        public Player ActivePlayer { get; }
+
+        public Spot[] Spots { get; }
+
+        public Game()
+        {
+            ActivePlayer = new Player("x");
+            Players = new Player[] {  ActivePlayer, new Player("o") };
+            Spots = CreateBoard();
+        }
+
+        private static Spot[] CreateBoard()
+        {
+            return new Spot[] {
+                new Spot(1,1), new Spot(1,2), new Spot(1,3),
+                new Spot(2,1), new Spot(2,2), new Spot(2,3),
+                new Spot(3,1), new Spot(3,2), new Spot(3,3)
+            };
+       
+        }
+    }
+
+    public class Player
+    {
+        public string Character { get; }
+
+        public Player(string character)
+        {
+            Character = character;
+        }
+    }
+
+    public class Spot
+    {
+        public int Row { get; set; }
+        public int Column { get; set; }
+
+        public Spot(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+
+        public Spot() { }
+
+        public override bool Equals(object obj)
+        {
+            if (this.Row == obj.Row && this.Column == obj.Column)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool Available => true;
+    }
+}
